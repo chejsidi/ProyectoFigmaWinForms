2efa0b8ab72e8a95e84914a6aa653850aff33850
Chej <chej.sidi.53388@ikasle.egibide.org>
2026-02-09 21:48:00 +0100

    Fix: Al abrir que abra con un tamaño definido

--- ProyectoFigma/Vista/GestorBiblioteca.Designer.cs
+++ ProyectoFigma/Vista/GestorBiblioteca.Designer.cs
@@ -29,26 +29,26 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanelFooter = new System.Windows.Forms.TableLayoutPanel();
-            this.pictureBoxLogoLetras2 = new System.Windows.Forms.PictureBox();
-            this.pictureBoxLogo2 = new System.Windows.Forms.PictureBox();
             this.labelCopy = new System.Windows.Forms.Label();
             this.tableLayoutPanelRedes = new System.Windows.Forms.TableLayoutPanel();
             this.labelRedes = new System.Windows.Forms.Label();
-            this.pictureBoxLogoRedes = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.pictureBoxLogoLetras1 = new System.Windows.Forms.PictureBox();
             this.labelPrestamos = new System.Windows.Forms.Label();
             this.labelLibros = new System.Windows.Forms.Label();
             this.labelUsuarios = new System.Windows.Forms.Label();
+            this.pictureBoxLogoLetras1 = new System.Windows.Forms.PictureBox();
             this.pictureBoxLogo1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxLogoLetras2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxLogo2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxLogoRedes = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanelFooter.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogoLetras2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo2)).BeginInit();
             this.tableLayoutPanelRedes.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogoRedes)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogoLetras1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogoLetras2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogoRedes)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanelFooter
@@ -67,38 +67,16 @@
             this.tableLayoutPanelFooter.Name = "tableLayoutPanelFooter";
             this.tableLayoutPanelFooter.RowCount = 1;
             this.tableLayoutPanelFooter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanelFooter.Size = new System.Drawing.Size(800, 53);
+            this.tableLayoutPanelFooter.Size = new System.Drawing.Size(940, 53);
             this.tableLayoutPanelFooter.TabIndex = 0;
-            // 
-            // pictureBoxLogoLetras2
-            // 
-            this.pictureBoxLogoLetras2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBoxLogoLetras2.Image = global::ProyectoFigma.Properties.Resources.logoLetras;
-            this.pictureBoxLogoLetras2.Location = new System.Drawing.Point(603, 3);
-            this.pictureBoxLogoLetras2.Name = "pictureBoxLogoLetras2";
-            this.pictureBoxLogoLetras2.Size = new System.Drawing.Size(194, 47);
-            this.pictureBoxLogoLetras2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBoxLogoLetras2.TabIndex = 11;
-            this.pictureBoxLogoLetras2.TabStop = false;
-            // 
-            // pictureBoxLogo2
-            // 
-            this.pictureBoxLogo2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBoxLogo2.Image = global::ProyectoFigma.Properties.Resources.logoSinLetras;
-            this.pictureBoxLogo2.Location = new System.Drawing.Point(3, 3);
-            this.pictureBoxLogo2.Name = "pictureBoxLogo2";
-            this.pictureBoxLogo2.Size = new System.Drawing.Size(194, 47);
-            this.pictureBoxLogo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBoxLogo2.TabIndex = 8;
-            this.pictureBoxLogo2.TabStop = false;
             // 
             // labelCopy
             // 
             this.labelCopy.AutoSize = true;
             this.labelCopy.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelCopy.Location = new System.Drawing.Point(203, 0);
+            this.labelCopy.Location = new System.Drawing.Point(238, 0);
             this.labelCopy.Name = "labelCopy";
-            this.labelCopy.Size = new System.Drawing.Size(194, 53);
+            this.labelCopy.Size = new System.Drawing.Size(229, 53);
             this.labelCopy.TabIndex = 9;
             this.labelCopy.Text = "© Ayuntamiento de Vitoria-Gasteiz";
             this.labelCopy.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -109,7 +87,7 @@
             this.tableLayoutPanelRedes.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanelRedes.Controls.Add(this.labelRedes, 0, 0);
             this.tableLayoutPanelRedes.Controls.Add(this.pictureBoxLogoRedes, 0, 1);
-            this.tableLayoutPanelRedes.Location = new System.Drawing.Point(403, 3);
+            this.tableLayoutPanelRedes.Location = new System.Drawing.Point(473, 3);
             this.tableLayoutPanelRedes.Name = "tableLayoutPanelRedes";
             this.tableLayoutPanelRedes.RowCount = 2;
             this.tableLayoutPanelRedes.RowStyles.Add(new System.Windows.Forms.RowStyle());
@@ -127,17 +105,6 @@
             this.labelRedes.TabIndex = 0;
             this.labelRedes.Text = "Síguenos en";
             this.labelRedes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // pictureBoxLogoRedes
-            // 
-            this.pictureBoxLogoRedes.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBoxLogoRedes.Image = global::ProyectoFigma.Properties.Resources.redesFooter;
-            this.pictureBoxLogoRedes.Location = new System.Drawing.Point(3, 16);
-            this.pictureBoxLogoRedes.Name = "pictureBoxLogoRedes";
-            this.pictureBoxLogoRedes.Size = new System.Drawing.Size(188, 27);
-            this.pictureBoxLogoRedes.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBoxLogoRedes.TabIndex = 1;
-            this.pictureBoxLogoRedes.TabStop = false;
             // 
             // tableLayoutPanel1
             // 
@@ -159,26 +126,15 @@
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 53);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(940, 53);
             this.tableLayoutPanel1.TabIndex = 1;
-            // 
-            // pictureBoxLogoLetras1
-            // 
-            this.pictureBoxLogoLetras1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBoxLogoLetras1.Image = global::ProyectoFigma.Properties.Resources.logoLetras;
-            this.pictureBoxLogoLetras1.Location = new System.Drawing.Point(539, 3);
-            this.pictureBoxLogoLetras1.Name = "pictureBoxLogoLetras1";
-            this.pictureBoxLogoLetras1.Size = new System.Drawing.Size(95, 47);
-            this.pictureBoxLogoLetras1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBoxLogoLetras1.TabIndex = 8;
-            this.pictureBoxLogoLetras1.TabStop = false;
             // 
             // labelPrestamos
             // 
             this.labelPrestamos.AutoSize = true;
             this.labelPrestamos.Dock = System.Windows.Forms.DockStyle.Fill;
             this.labelPrestamos.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.labelPrestamos.Location = new System.Drawing.Point(431, 0);
+            this.labelPrestamos.Location = new System.Drawing.Point(501, 0);
             this.labelPrestamos.Name = "labelPrestamos";
             this.labelPrestamos.Size = new System.Drawing.Size(102, 53);
             this.labelPrestamos.TabIndex = 4;
@@ -191,7 +147,7 @@
             this.labelLibros.AutoSize = true;
             this.labelLibros.Dock = System.Windows.Forms.DockStyle.Fill;
             this.labelLibros.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.labelLibros.Location = new System.Drawing.Point(363, 0);
+            this.labelLibros.Location = new System.Drawing.Point(433, 0);
             this.labelLibros.Name = "labelLibros";
             this.labelLibros.Size = new System.Drawing.Size(62, 53);
             this.labelLibros.TabIndex = 3;
@@ -204,7 +160,7 @@
             this.labelUsuarios.AutoSize = true;
             this.labelUsuarios.Dock = System.Windows.Forms.DockStyle.Fill;
             this.labelUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.labelUsuarios.Location = new System.Drawing.Point(272, 0);
+            this.labelUsuarios.Location = new System.Drawing.Point(342, 0);
             this.labelUsuarios.Name = "labelUsuarios";
             this.labelUsuarios.Size = new System.Drawing.Size(85, 53);
             this.labelUsuarios.TabIndex = 2;
@@ -212,22 +168,66 @@
             this.labelUsuarios.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.labelUsuarios.Click += new System.EventHandler(this.labelUsuarios_Click);
             // 
+            // pictureBoxLogoLetras1
+            // 
+            this.pictureBoxLogoLetras1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxLogoLetras1.Image = global::ProyectoFigma.Properties.Resources.logoLetras;
+            this.pictureBoxLogoLetras1.Location = new System.Drawing.Point(609, 3);
+            this.pictureBoxLogoLetras1.Name = "pictureBoxLogoLetras1";
+            this.pictureBoxLogoLetras1.Size = new System.Drawing.Size(95, 47);
+            this.pictureBoxLogoLetras1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogoLetras1.TabIndex = 8;
+            this.pictureBoxLogoLetras1.TabStop = false;
+            // 
             // pictureBoxLogo1
             // 
             this.pictureBoxLogo1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBoxLogo1.Image = global::ProyectoFigma.Properties.Resources.logoSinLetras;
-            this.pictureBoxLogo1.Location = new System.Drawing.Point(166, 3);
+            this.pictureBoxLogo1.Location = new System.Drawing.Point(236, 3);
             this.pictureBoxLogo1.Name = "pictureBoxLogo1";
             this.pictureBoxLogo1.Size = new System.Drawing.Size(100, 47);
             this.pictureBoxLogo1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBoxLogo1.TabIndex = 7;
             this.pictureBoxLogo1.TabStop = false;
             // 
+            // pictureBoxLogoLetras2
+            // 
+            this.pictureBoxLogoLetras2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxLogoLetras2.Image = global::ProyectoFigma.Properties.Resources.logoLetras;
+            this.pictureBoxLogoLetras2.Location = new System.Drawing.Point(708, 3);
+            this.pictureBoxLogoLetras2.Name = "pictureBoxLogoLetras2";
+            this.pictureBoxLogoLetras2.Size = new System.Drawing.Size(229, 47);
+            this.pictureBoxLogoLetras2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogoLetras2.TabIndex = 11;
+            this.pictureBoxLogoLetras2.TabStop = false;
+            // 
+            // pictureBoxLogo2
+            // 
+            this.pictureBoxLogo2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxLogo2.Image = global::ProyectoFigma.Properties.Resources.logoSinLetras;
+            this.pictureBoxLogo2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBoxLogo2.Name = "pictureBoxLogo2";
+            this.pictureBoxLogo2.Size = new System.Drawing.Size(229, 47);
+            this.pictureBoxLogo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogo2.TabIndex = 8;
+            this.pictureBoxLogo2.TabStop = false;
+            // 
+            // pictureBoxLogoRedes
+            // 
+            this.pictureBoxLogoRedes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxLogoRedes.Image = global::ProyectoFigma.Properties.Resources.redesFooter;
+            this.pictureBoxLogoRedes.Location = new System.Drawing.Point(3, 16);
+            this.pictureBoxLogoRedes.Name = "pictureBoxLogoRedes";
+            this.pictureBoxLogoRedes.Size = new System.Drawing.Size(188, 27);
+            this.pictureBoxLogoRedes.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogoRedes.TabIndex = 1;
+            this.pictureBoxLogoRedes.TabStop = false;
+            // 
             // GestorBiblioteca
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(940, 450);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.tableLayoutPanelFooter);
             this.IsMdiContainer = true;
@@ -235,15 +235,15 @@
             this.Text = "Form1";
             this.tableLayoutPanelFooter.ResumeLayout(false);
             this.tableLayoutPanelFooter.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogoLetras2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo2)).EndInit();
             this.tableLayoutPanelRedes.ResumeLayout(false);
             this.tableLayoutPanelRedes.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogoRedes)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogoLetras1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogoLetras2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogoRedes)).EndInit();
             this.ResumeLayout(false);
 
         }
